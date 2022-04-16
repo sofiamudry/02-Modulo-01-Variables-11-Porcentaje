77674a8ae95f001dbd584420643c8a57dec746fc
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 14:11:11 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('porcentaje');
+
+alert('Hola, Bienvenide!');
+
+const numero = prompt ('Ingrese el numero');
+const porcentaje = prompt ('Ingrese el porcentaje que desea obtener');
+const porcentual = (numero*porcentaje/100);
+
+alert(`El porcentaje es ${porcentual}`);
